62b1022fe6af67a19362ab421bc1d824e9bd7627
Vyacheslav Zhuravskiy <magic.in.our.world@gmil.com>
2016-10-24 22:25:10 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,31 +1,83 @@
 window.onload = function () {
     var scramble = document.getElementById("scramble");
-    // var selectedEvent = document.getElementById("selectedEvent");
+    var selectedEvent = document.getElementById("selectedEvent");
 }
 
 var generateScramble = function () {
     var newScramble = '';
     var prevMove = '';
     var prevPrevMove = '';
-    var moves = ["U", "D", "R", "L", "F", "B", "U\'", "D\'", "R\'", "L\'", "F\'", "B\'", "U2", "D2", "R2", "L2", "F2", "B2"];
+    if (selectedEvent.options[ selectedEvent.selectedIndex ].value == "3x3") {
+        var moves = ["U", "D", "R", "L", "F", "B", "U\'", "D\'", "R\'", "L\'", "F\'", "B\'", "U2", "D2", "R2", "L2", "F2", "B2"];
 
-    for (var i = 0; i < 20; i++) {
-        var randomInt = Math.floor((Math.random() * moves.length));
-        move = moves[randomInt];
-        if (prevMove == 'U' & prevPrevMove != 'D' || prevMove == 'D' && prevPrevMove != 'U' || prevMove == 'L' && prevPrevMove != 'R' || prevMove == 'R' && prevPrevMove != 'L' || prevMove == 'F' && prevPrevMove != 'B' || prevMove == 'B' && prevPrevMove != 'F') {
-            prevPrevMove = '';
-        }
-        while (move.charAt(0) == prevMove || move.charAt(0) == prevPrevMove) {
+        for (var i = 0; i < 20; i++) {
             var randomInt = Math.floor((Math.random() * moves.length));
             move = moves[randomInt];
+            if (prevMove == 'U' & prevPrevMove != 'D' || prevMove == 'D' && prevPrevMove != 'U' || prevMove == 'L' && prevPrevMove != 'R' || prevMove == 'R' && prevPrevMove != 'L' || prevMove == 'F' && prevPrevMove != 'B' || prevMove == 'B' && prevPrevMove != 'F') {
+                prevPrevMove = '';
+            }
+            while (move.charAt(0) == prevMove || move.charAt(0) == prevPrevMove) {
+                var randomInt = Math.floor((Math.random() * moves.length));
+                move = moves[randomInt];
+            }
+            prevPrevMove = prevMove.charAt(0);
+            prevMove = move.charAt(0);
+            newScramble = newScramble + move + ' ';
+            scramble.innerHTML = newScramble;
         }
-        prevPrevMove = prevMove.charAt(0);
-        prevMove = move.charAt(0);
-        newScramble = newScramble + move + ' ';
-        scramble.innerHTML = newScramble;
     }
-}
+    else if (selectedEvent.options[ selectedEvent.selectedIndex ].value == "2x2") {
+        var moves = ["U", "R", "F", "U\'", "R\'", "F\'", "U2", "R2", "F2"];
 
+        for (var i = 0; i < 11; i++) {
+            var randomInt = Math.floor((Math.random() * moves.length));
+            move = moves[randomInt];
+            while (move.charAt(0) == prevMove) {
+                var randomInt = Math.floor((Math.random() * moves.length));
+                move = moves[randomInt];
+            }
+            prevMove = move.charAt(0);
+            newScramble = newScramble + move + ' ';
+            scramble.innerHTML = newScramble;
+        }
+    }
+    else if (selectedEvent.options[ selectedEvent.selectedIndex ].value == "Skewb") {
+        var moves = ["U", "R", "F", "U\'", "R\'", "F\'"];
+
+        for (var i = 0; i < 9; i++) {
+            var randomInt = Math.floor((Math.random() * moves.length));
+            move = moves[randomInt];
+            while (move.charAt(0) == prevMove) {
+                var randomInt = Math.floor((Math.random() * moves.length));
+                move = moves[randomInt];
+            }
+            prevMove = move.charAt(0);
+            newScramble = newScramble + move + ' ';
+            scramble.innerHTML = newScramble;
+        }
+    }
+    // else {
+    //     rMoves = ["R++ ", "R-- "];
+    //     dMoves = ["D++ ", "D-- "];
+    //     uMoves = ["U<br>", "U\'<br>"];
+
+    //     for (var i = 0; i < 7; i++) {
+    //         for (var k = 0; k < 10; k++) {
+    //             if (k % 2 == 0) {
+    //                 var randomInt = Math.floor((Math.random() * rMoves.length));
+    //                 newScramble += rMoves[randomInt];
+    //             }
+    //             else {
+    //                 var randomInt = Math.floor((Math.random() * dMoves.length));
+    //                 newScramble += dMoves[randomInt];
+    //             }
+    //         }
+    //         var randomInt = Math.floor((Math.random() * uMoves.length));
+    //         newScramble += uMoves[randomInt];
+    //     }
+    //     scramble.innerHTML = newScramble;
+    // }
+}
 
 function trim(string) { return string.replace (/\s+/g, " ").replace(/(^\s*)|(\s*)$/g, ''); }
 var init = 0;
